b0fbe28e08cd473c864cecaec90d3faae0593d7a
José Airton <airtonsmorais28@gmail.com>
2021-02-01 21:56:35 -0300

    added routines the delete and update

--- src/main/java/managedBean/UsuarioPessoaManagedBean.java
+++ src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -1,7 +1,12 @@
 package managedBean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import dao.DaoGeneric;
 import model.UsuarioPessoa;
@@ -12,6 +17,7 @@ public class UsuarioPessoaManagedBean {
 
 	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
 	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+	private List<UsuarioPessoa> list = new ArrayList<UsuarioPessoa>();
 
 	public UsuarioPessoa getUsuarioPessoa() {
 		return usuarioPessoa;
@@ -20,15 +26,31 @@ public class UsuarioPessoaManagedBean {
 	public void setUsuarioPessoa(UsuarioPessoa usuarioPessoa) {
 		this.usuarioPessoa = usuarioPessoa;
 	}
-	
-	public String salvar() {
-		daoGeneric.salvar(usuarioPessoa);	
-		return"";
+
+	public List<UsuarioPessoa> getList() {
+		list = daoGeneric.listar(UsuarioPessoa.class);
+		return list;
 	}
-	
+
+	public String salvar() {
+		daoGeneric.salvar(usuarioPessoa);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação", "Salvo com sucesso!!"));
+		return "";
+	}
+
 	public String novo() {
 		usuarioPessoa = new UsuarioPessoa();
-		return"";
+		return "";
+	}
+
+	public String deletar() {
+		daoGeneric.deletarPoId(usuarioPessoa);
+		usuarioPessoa = new UsuarioPessoa();
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação", "Salvo com sucesso!!"));
+		return "";
+
 	}
 
 }

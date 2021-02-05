87a133f11d3b18f70b17f2bf0674b910edf4cb28
José Airton <airtonsmorais28@gmail.com>
2021-02-05 20:42:27 -0300

    implementing telefone user

--- src/main/java/managedBean/TelefoneManagedBean.java
+++ src/main/java/managedBean/TelefoneManagedBean.java
@@ -1,10 +1,17 @@
 package managedBean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
+import dao.DaoTelefone;
+import dao.DaoUsuario;
+import model.TelefoneUser;
 import model.UsuarioPessoa;
 
 @ManagedBean(name = "telefoneManaged")
@@ -12,14 +19,67 @@ import model.UsuarioPessoa;
 public class TelefoneManagedBean {
 
 	private UsuarioPessoa user = new UsuarioPessoa();
+	private DaoUsuario<UsuarioPessoa> daoUser = new DaoUsuario<UsuarioPessoa>();
+	private DaoTelefone<TelefoneUser> daoTelefone = new DaoTelefone<TelefoneUser>();
+	List<TelefoneUser> list = new ArrayList<TelefoneUser>();
+
+	private TelefoneUser telefone = new TelefoneUser();
 
 	@PostConstruct
 	public void init() {
 
 		String coduser = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
 				.get("codigouser");
-		System.out.println(coduser);
+		user = daoUser.pesquisar(Long.parseLong(coduser), UsuarioPessoa.class);
 
+	}
+
+	public void setTelefone(TelefoneUser telefone) {
+		this.telefone = telefone;
+	}
+
+	public TelefoneUser getTelefone() {
+		return telefone;
+	}
+
+	public void setUser(UsuarioPessoa user) {
+		this.user = user;
+	}
+
+	public UsuarioPessoa getUser() {
+		return user;
+	}
+
+	public void setDaoTelefone(DaoTelefone<TelefoneUser> daoTelefone) {
+		this.daoTelefone = daoTelefone;
+	}
+
+	public void setDaoUser(DaoUsuario<UsuarioPessoa> daoUser) {
+		this.daoUser = daoUser;
+	}
+	
+	public List<TelefoneUser> getList() {
+		list = daoTelefone.listar(TelefoneUser.class);
+		return list;
+	}
+	
+	public void setList(List<TelefoneUser> list) {
+		this.list = list;
+	}
+
+	public String salvar() {
+		telefone.setUsuarioPessoa(user);
+		daoTelefone.salvar(telefone);
+		telefone = new TelefoneUser();
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Cadastrado com sucesso!!!"));
+		
+		return "";
+	}
+	
+	public void deletar() {
+		daoTelefone.deletarPoId(getTelefone());
+		telefone = new TelefoneUser();
 	}
 
 }
